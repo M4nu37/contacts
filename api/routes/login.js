0b6f08da353761d5c7bf472090fea91c52ad60c0
Manuel Sampablo <66753539+M4nu37@users.noreply.github.com>
2021-04-14 21:29:58 +0200

    Update login.js

--- api/routes/login.js
+++ api/routes/login.js
@@ -7,15 +7,15 @@ const user = require('../models/user_model');
 
 router.use(express.urlencoded({ extended: true }));
 
-// Handling the GET requests
-router.get('/:name/:pass', (req, res) => {
+// Handling POST requests
+router.post('/:name/:pass', (req, res) => {
     
     // Get the ULR parameters 
     let userName = req.params.name;
     let userPass = req.params.pass;
 
     try {
-        // MongoDB connection to check if the user exists and log him in
+        // MongoDB connection to insert the new user into the database
         async function run() {
 
             // Stablish connection with Mongo via mongoose
@@ -24,8 +24,8 @@ router.get('/:name/:pass', (req, res) => {
                 useUnifiedTopology: true
             });
             
-            // defining the user schema and model from:        
-            const User = user;  // models/user_model.js
+            // Defining the user schema and model from:
+            const User = user;  // models/user_model.js 
             
 
             // Check if the user already exists in the collection
@@ -36,33 +36,39 @@ router.get('/:name/:pass', (req, res) => {
             
             (err, data) => {
                 if (err) throw err;
-                
-                // Send '404 not found' error message if the user is not registered
+
+                console.log(data)
+
+                // Insert the new user to the collection and send a 'created' message
                 if (data.length === 0) {
-                    res.status(404).json({
-                        message: '404 not found'
+                    User.create({ 
+                        name: userName,
+                        password: userPass,
+                        contacts: []
+                    });   
+                    res.status(201).json({
+                        message: 'User was created'
                     });
                     res.end();
                 }
-
-                // If the user is in the DB, then send an 'accepted' message
+                
+                // Send '500' error message and status if the user exists
                 else {
-                    res.status(202).json({
-                        message: 'Accepted'
+                    res.status(500).json({
+                        message: 'The user already exists'
                     });
-                    console.log(userName + ' loged in');
                     res.end();
                 }
-            }); 
+            });
             
 
-            
-            // Check if either the user is a Student or a Professor and register it
-        }
+
+            // Check if either the user is a Student or a Professor and register it           
+        }       
+
         // Run the connection
         run();
-    }
-        
+    } 
     // In case that's any error, throw it
     catch (e) {
         console.log(e);
