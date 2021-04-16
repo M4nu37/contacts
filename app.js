8b2c08b0c2ee6c0ffffb59c88272ece9a17d276d
Manuel Sampablo <66753539+M4nu37@users.noreply.github.com>
2021-04-16 21:12:14 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,18 +4,20 @@ const app = express();
 // Import routes
 const registerRoute = require('./api/routes/register');
 const loginRoute = require('./api/routes/login');
-const contactRoute = require('./api/routes/contact');
+const emailRoute = require('./api/routes/email');
 const mainRoute = require('./api/routes/main');
 const addContactRoute = require('./api/routes/addContact');
 const deleteContactRoute = require('./api/routes/deleteContact');
+const editContactRoute = require('./api/routes/editContact');
 
 
 // Use the routes
 app.use('/register', registerRoute);
 app.use('/login', loginRoute);
-app.use('/sendMail', contactRoute);
+app.use('/sendMail', emailRoute);
 app.use('/main', mainRoute);
 app.use('/addContact', addContactRoute);
 app.use('/deleteContact', deleteContactRoute);
+app.use('/editContact', editContactRoute);
 
 module.exports = app;
